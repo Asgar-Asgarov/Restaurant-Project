7308582ecd6c223a8dfc4132599fc2e2e2ad219e
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-07-01 16:17:29 +0400

    feature:responsive hamburger menu window style added

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -25,6 +25,20 @@ const Navbar = () => (
     Book Table
     </a>
    </div>
+   <div className="app__navbar-smallscreen">
+    <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{}}/>
+    <div className="app__navbar-smallscreen_overlay flex_center slide_bottom">
+      <MdOutlineRestaurantMenu fontSize={27} className="overlay_close" onClick={()=>{}}/>
+      <ul className="app__navbar-smallscreen_links">
+    <li className="p__opensans"><a href="#home">Home</a></li>
+    <li className="p__opensans"><a href="#about">About</a></li>
+    <li className="p__opensans"><a href="#menu">Menu</a></li>
+    <li className="p__opensans"><a href="#awards">Awards</a></li>
+    <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+
+   </div>
   </nav>
 );
 
